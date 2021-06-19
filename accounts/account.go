83671cdfb8cd1657beaa1f8a25a071c0e6722869
jyebe9034 <jyebe9034@gmail.com>
2021-06-19 15:35:47 +0900

    add method and error

--- accounts/account.go
+++ accounts/account.go
@@ -1,11 +1,35 @@
 package accounts
 
-type Accounts struct {
+import "errors"
+
+// Accounts struct
+type Account struct {
 	owner   string
 	balance int
 }
 
-func NewAccount(owner string) *Accounts {
-	account := Accounts{owner: owner, balance: 0}
+var errNoMoney = errors.New("can't withdraw cuz you're poor")
+
+// NewAccount create account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) { // (a Account)는 리시버라고 불림
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balnace() int {
+	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/jyebe9034/golearning/accounts"
 )
 
 func main() {
-	accounts := accounts.NewAccount("hannah")
-	fmt.Println(accounts)
+	account := accounts.NewAccount("hannah")
+	account.Deposit(10)
+	fmt.Println(account.Balnace())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balnace())
 }
