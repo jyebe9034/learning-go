de595cfb5abd1ceb2d8db4655f51855a3d8bea3c
jyebe9034 <jyebe9034@gmail.com>
2021-06-24 22:17:50 +0900

    change main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/jyebe9034/golearning/accounts"
 )
@@ -10,10 +9,10 @@ import (
 func main() {
 	account := accounts.NewAccount("hannah")
 	account.Deposit(10)
-	fmt.Println(account.Balnace())
-	err := account.Withdraw(20)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(account.Balnace())
+	// err := account.Withdraw(20)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(account.Balnace(), account.Owner())
+	fmt.Println(account)
 }
